15fda5e688b9357e220657f81cc47de7f8694275
jerson100 <juamkoo@gmail.com>
2020-07-21 05:11:01 -0500

    Add docs

---
+++ docs/precache-manifest.0431c599a565d8aa19b4ee28fae514aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72721ec959fbf625f21903d2c1cfd19b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "394a45268d905bd259f3",
+    "url": "/static/css/main.161b17a4.chunk.css"
+  },
+  {
+    "revision": "90fa7e0eb6d34d2b5b13",
+    "url": "/static/js/2.34b52178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34b52178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394a45268d905bd259f3",
+    "url": "/static/js/main.0a7abb7b.chunk.js"
+  },
+  {
+    "revision": "28b2448ea3201b2f979f",
+    "url": "/static/js/runtime-main.09460a5c.js"
+  }
+]);
